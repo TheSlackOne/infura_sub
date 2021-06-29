2c56b6bce301db4d27961f4aef3ec798b8cd5240
Ernesto Messina <ernesto.messina@digitalbeings.net>
2021-06-29 10:09:32 -0300

    Detecting transactions and tokens.

--- tx_sub.js
+++ tx_sub.js
@@ -3,6 +3,7 @@ const Web3 = require('web3');
 function watchEtherTransfers() {
   // Instantiate web3 with WebSocket provider
   const network = "rinkeby";
+  //const network = "mainnet";
   const project_id = "51bcdbf8373f4cd58101860564fa0762";
   const url = `wss://${network}.infura.io/ws/v3/${project_id}`;
   let web3 = new Web3(new Web3.providers.WebsocketProvider(url));
@@ -16,23 +17,25 @@ function watchEtherTransfers() {
   })
   .on('data', async (txHash) => {
     try {
-      console.log('Transaction hash is: ' + txHash + '\n')
-      
       // Instantiate web3 with HttpProvider
-      const web3Http = new Web3('https://rinkeby.infura.io/v3/51bcdbf8373f4cd58101860564fa0762')
+      const web3Http = new Web3(`https://${network}.infura.io/v3/${project_id}`)
 
       // Get transaction details
       const trx = await web3Http.eth.getTransaction(txHash)
-      //console.log(`Transaction details: ${JSON.stringify(trx)}`)
+      
+      const json_obj = JSON.parse(JSON.stringify(trx))
+      if (json_obj?.input && json_obj?.input !== '0x') {
+        console.log(`Transaction hash is: ${txHash}`)
+        //console.log(`Payload: ${JSON.stringify(trx)}`);
+        json_obj.from ?? console.log(`From: ${json_obj.from}`)
+        json_obj.to ?? console.log(`To: ${json_obj.to}`);
+        console.log(`data: ${json_obj.input}`);
+      }
 
       //const valid = validateTransaction(trx)
       // If transaction is not valid, simply return
       //if (!valid) return
 
-      const obj = JSON.parse(JSON.stringify(trx))
-      console.log(`From: ${obj?.from}`);
-      console.log(`To: ${obj?.to}`);
-      
       // Initiate transaction confirmation
       //confirmEtherTransaction(txHash)
 
